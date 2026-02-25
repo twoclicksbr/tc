fbf989ed068ab3fd27e9f349d2bc5f06922a5262
Alex Bethel <github@twoclicks.com.br>
2026-02-25 04:09:30 -0300

    fix: remove duplicate Export button

--- frontend/src/layouts/demo3/layout.tsx
+++ frontend/src/layouts/demo3/layout.tsx
@@ -1,18 +1,15 @@
 import { useEffect } from 'react';
-import { Download } from 'lucide-react';
 import { Helmet } from 'react-helmet-async';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { MENU_SIDEBAR } from '@/config/menu.config';
 import { useBodyClass } from '@/hooks/use-body-class';
 import { useMenu } from '@/hooks/use-menu';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { useSettings } from '@/providers/settings-provider';
-import { Button } from '@/components/ui/button';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Navbar } from './components/navbar';
 import { Sidebar } from './components/sidebar';
-import { Toolbar, ToolbarActions, ToolbarHeading } from './components/toolbar';
 
 export function Demo3Layout() {
   const { pathname } = useLocation();
@@ -50,19 +47,6 @@ export function Demo3Layout() {
           <div className="flex grow rounded-b-xl bg-background border-x border-b border-border lg:mt-(--navbar-height) mx-5 lg:ms-(--sidebar-width) mb-5">
             <div className="flex flex-col grow kt-scrollable-y lg:[scrollbar-width:auto] pt-7 lg:[&_[data-slot=container]]:pe-2">
               <main className="grow" role="content">
-                {pathname !== '/' && (
-                  <Toolbar>
-                    <ToolbarHeading />
-                    <ToolbarActions>
-                      <Button variant="outline" size="sm" asChild>
-                        <Link to={'/account/home/get-started'}>
-                          <Download />
-                          Export
-                        </Link>
-                      </Button>
-                    </ToolbarActions>
-                  </Toolbar>
-                )}
                 <Outlet />
               </main>
               <Footer />
