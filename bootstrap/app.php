ccf65dfa05bc8f4b6a719a845efa8fff38f34de2
Alex Bethel <github@twoclicks.com.br>
2026-02-25 03:31:15 -0300

    fix: return 401 JSON for unauthenticated requests instead of 500

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,5 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        });
     })->create();
